9995efeb3d56dc57f0de16bd21e5bd93dd057661
中村裕子 <nakamurayuko@nakamurayuukonoMacBook-puro.local>
2019-04-04 21:12:52 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a1db2a2fb8b900f242ed09e8ad9fdeceb193d50f4dea9e0aaa554b66f93514d87eea91ba6fdc5b3b7cac9d76602a14a91ee06c1f3ef423d12e732c86b66802d1'
